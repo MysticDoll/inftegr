d816846c969f520da3e1c8fb8836474bc42b4031
Mitsuru Takigahira <mysticdoll@mysticdoll.com>
2022-09-16 16:45:13 +0900

    update to windows-rs 0.39.0 and add ocr functions

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use windows::ApplicationModel::Core::{
     CoreApplication, CoreApplicationView, IFrameworkView, IFrameworkViewSource,
 };
 use windows::Foundation::*;
-use windows::Graphics::{Capture::*, DirectX::Direct3D11::*, DirectX::*, DisplayId};
+use windows::Graphics::{Capture::*, DirectX::Direct3D11::*, DirectX::*, DisplayId, Imaging::SoftwareBitmap};
 use windows::Win32::Foundation::*;
 use windows::Win32::Graphics::{Direct3D::*, Direct3D11::*, Dxgi::*};
 use windows::Win32::System::ProcessStatus::*;
@@ -13,6 +13,7 @@ use windows::Win32::System::{Com::*, WinRT::Direct3D11::*};
 use windows::Win32::UI::Shell::*;
 use windows::Win32::UI::WindowsAndMessaging::*;
 use windows::UI::Core::*;
+use windows::Media::Ocr::OcrEngine;
 
 use std::collections::HashSet;
 use std::sync::{Arc, Mutex};
@@ -23,28 +24,35 @@ use crate::directx::*;
 
 mod directx;
 
-fn main() -> Result<()> {
-    let mut store: HashSet<(HWND, String, String)> = HashSet::new();
+static mut global_hwnd: HWND = HWND(0);
+
+#[tokio::main]
+async fn main() -> Result<()> {
     unsafe {
         CoInitializeEx(std::ptr::null_mut(), COINIT_MULTITHREADED)?;
         //RoInitialize(RO_INIT_SINGLETHREADED)?;
         EnumWindows(
             Some(enum_window_proc),
-            std::ptr::addr_of_mut!(store) as isize,
+            LPARAM(0)
         );
     }
 
-    let hwnd = store
-        .iter()
-        .find(|(hwnd, window_text, exec_name)| {
-            //println!(
-            //    "HWND: {}, Window Title: {}, Executable: {}",
-            //    hwnd, window_text, exec_name
-            //);
-            exec_name.contains("th185.exe")
-        })
-        .unwrap()
-        .0;
+    let hwnd = unsafe { global_hwnd };
+
+    if hwnd == HWND(0) {
+        panic!("couldn't found target window handle");
+    }
+
+    //let hwnd = HWND(store
+    //    .iter()
+    //    .find(|(hwnd, window_text, exec_name)| {
+    //        //println!(
+    //        //    "HWND: {}, Window Title: {}, Executable: {}",
+    //        //    hwnd, window_text, exec_name
+    //        //);
+    //        exec_name.contains("th185.exe")
+    //    })
+    //    .unwrap().0);
     //create_window()?;
 
     let d3device: IDirect3DDevice = create_device()?;
@@ -55,7 +63,7 @@ fn main() -> Result<()> {
         windows::Win32::System::WinRT::Graphics::Capture::IGraphicsCaptureItemInterop,
     >()?;
     println!("create_item");
-    let gcitem: GraphicsCaptureItem = unsafe { interop.CreateForWindow(&hwnd)? };
+    let gcitem: GraphicsCaptureItem = unsafe { interop.CreateForWindow(*&hwnd)? };
     println!("create_item_done");
 
     /*
@@ -69,7 +77,7 @@ fn main() -> Result<()> {
 
     println!("create_framepool");
     let frame_pool = Direct3D11CaptureFramePool::Create(
-        d3device,
+        &d3device,
         DirectXPixelFormat::B8G8R8A8UIntNormalized,
         2,
         (&gcitem).Size()?,
@@ -77,12 +85,33 @@ fn main() -> Result<()> {
     .unwrap();
 
     println!("create capture session");
-    let session = frame_pool.CreateCaptureSession(gcitem).unwrap();
+    let session = frame_pool.CreateCaptureSession(&gcitem).unwrap();
     println!("start capture");
     let _r = session.StartCapture();
     println!("capture started");
 
-    loop {}
+    let ocrEngine = OcrEngine::TryCreateFromUserProfileLanguages().unwrap();
+
+    loop {
+        if let Ok(iaops_bitmap) = frame_pool.TryGetNextFrame()
+            .and_then(|frame| frame.Surface())
+            .and_then(|surface| SoftwareBitmap::CreateCopyFromSurfaceAsync(&surface)) {
+                if let Ok(bitmap) = iaops_bitmap.await {
+                    if let Ok(iaops_ocrResult) = ocrEngine.RecognizeAsync(&bitmap) {
+                        let ocrResult = iaops_ocrResult.await;
+                        if let Ok(lines) = ocrResult
+                            .and_then(|r| r.Lines())
+                            .map(|v| v.into_iter()) {
+                                for line in lines {
+                                    println!("{:?}", line.Text());
+                                }
+                            };
+                    };
+                };
+            }
+
+        tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
+    }
 
     Ok(())
 }
@@ -92,35 +121,40 @@ extern "system" fn enum_window_proc(
     lp: windows::Win32::Foundation::LPARAM,
 ) -> windows::Win32::Foundation::BOOL {
     unsafe {
-        let mut store = &mut *(lp as *mut HashSet<(HWND, String, String)>);
-        let mut pid = 0;
-        GetWindowThreadProcessId(&hwnd, &mut pid);
-        let handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, pid);
-        let mut module = [0; 1024];
-        K32EnumProcessModules(&handle, module.as_mut_ptr(), 1024, std::ptr::null_mut());
-        let mut raw_exec_name = windows::Win32::Foundation::PSTR([0; 1024].as_mut_ptr());
-        let exec_name_len = K32GetModuleFileNameExA(&handle, module[0], raw_exec_name, 1024);
-        let exec_name = std::str::from_utf8(std::slice::from_raw_parts(
-            raw_exec_name.0,
-            exec_name_len.try_into().unwrap(),
-        ));
+        // let mut store = &mut *(lp as *mut HashSet<(HWND, String, String)>);
+        //let mut store: HWND = std::mem::transmute(lp);
+        if global_hwnd == HWND(0) {
+            let mut pid = 0;
+            GetWindowThreadProcessId(hwnd, &mut pid);
+            if let Ok(handle) = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, pid) {
+                let mut module = HINSTANCE(0);
+                K32EnumProcessModules(handle, &mut module, 1024, std::ptr::null_mut());
 
-        let mut raw_window_text = windows::Win32::Foundation::PSTR([0; 1024].as_mut_ptr());
-        let window_text_len = GetWindowTextA(hwnd, raw_window_text, 1024);
-        let window_text = std::str::from_utf8(std::slice::from_raw_parts(
-            raw_window_text.0,
-            window_text_len.try_into().unwrap(),
-        ));
-        //println!(
-        //    "HWND: {:?}, Executable: {:?}, TEXT_LEN: {:?}",
-        //    hwnd, exec_name, file_name_len
-        //);
+                let mut raw_exec_name = [0; 1024];
+                let exec_name_len = K32GetModuleFileNameExA(handle, module, &mut raw_exec_name);
+                let exec_name = std::str::from_utf8(&raw_exec_name);
 
-        store.insert((
-            hwnd,
-            window_text.unwrap_or("unknown").to_owned(),
-            exec_name.unwrap_or("unknown").to_owned(),
-        ));
+                let mut raw_window_text = [0; 1024];
+                let window_text_len = GetWindowTextA(hwnd, &mut raw_window_text);
+                let window_text = std::str::from_utf8(&raw_window_text);
+                //println!(
+                //    "HWND: {:?}, Executable: {:?}, TEXT_LEN: {:?}",
+                //    hwnd, exec_name, exec_name_len
+                //);
+
+                // store.insert((
+                //    hwnd.0,
+                //    window_text.unwrap_or("unknown").to_owned(),
+                //    exec_name.unwrap_or("unknown").to_owned(),
+                // ));
+
+                if exec_name.unwrap_or("__unkonwn__").contains("th185.exe") {
+                    println!("hwnd: {:?}, exec_name: {:?}", hwnd, exec_name.unwrap_or(""));
+                    global_hwnd= hwnd;
+                    println!("stored: {:?}", global_hwnd);
+                };
+            }
+        }
     }
     true.into()
 }
